c65cba0aa820a40bc48729727274da92a7c60cce
sushil <np.msushil@gmail.com>
2022-05-20 22:35:18 +0545

    initialize spring boot project

---
+++ src/main/java/com/msucil/dev/springbase/SpringBaseApplication.java
@@ -0,0 +1,13 @@
+package com.msucil.dev.springbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msucil/dev/springbase/SpringBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msucil.dev.springbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
