af83f987ee4a7a3798dc0f7c7361f8e3a58a6960
sushil <np.msushil@gmail.com>
2022-06-20 16:10:02 +0545

    Add comment for empty block

--- src/main/java/com/msucil/dev/springbase/service/user/UserExistException.java
+++ src/main/java/com/msucil/dev/springbase/service/user/UserExistException.java
@@ -3,6 +3,7 @@ package com.msucil.dev.springbase.service.user;
 public class UserExistException extends RuntimeException {
 
     public UserExistException() {
+        // default constructor
     }
 
     public UserExistException(String message) {
